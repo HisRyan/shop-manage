e2d912a28699f0cb95a5028c0fb3fa37468b0b2f
lhj <659341328@qq.com>
2021-03-30 09:10:00 +0800

    feat: '登录'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: {name: 'signup'}
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/signup',
+    name : 'signup',
+    component: () => import('../views/signup/index.vue')
+  },
+  {
+    path: '/register',
+    name : 'register',
+    component: () => import('../views/signup/register.vue')
   }
+ 
 ]
 
 const router = createRouter({
