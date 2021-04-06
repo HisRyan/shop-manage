b7de1221296708e75d1abf08f7932fe19a0fe8c3
李小杰 <659341328@qq.com>
2021-04-06 20:55:02 +0800

    提交

---
+++ src/views/form/index.vue
@@ -0,0 +1,25 @@
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+const form = defineComponent({ 
+   setup() { 
+
+     return{  
+      }
+ }
+
+})
+
+export default form
+
+</script>
+
+<template>
+   <div>
+     自定义表单
+   </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/upload/index.vue
@@ -0,0 +1,23 @@
+<script lang='ts'>
+import { defineComponent } from "vue";
+import { Empty } from "ant-design-vue";
+const upload = defineComponent({
+  components: {
+    "a-empty":Empty
+  },
+  setup() {
+    return {};
+  },
+});
+
+export default upload;
+</script>
+
+<template>
+  <div>
+      <a-empty />
+  </div>
+</template>
+
+<style  lang=scss scoped>
+</style>
